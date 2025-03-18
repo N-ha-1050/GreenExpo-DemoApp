e0867422dc5aea12a70a4f8c8b24bd20b2971033
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2025-03-18 11:49:03 +0900

    🩹 システムプロンプトの修正

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -5,11 +5,16 @@ import { streamText } from "ai"
 export const maxDuration = 30
 
 export async function POST(req: Request) {
-    const { messages } = await req.json()
+    const { messages, systemPrompt } = await req.json()
+
+    // const system = systemPrompt
+    //     ? { role: "system", content: systemPrompt }
+    //     : undefined
 
     const result = streamText({
         model: google("gemini-1.5-flash"),
         messages,
+        system: systemPrompt,
     })
 
     return result.toDataStreamResponse()
